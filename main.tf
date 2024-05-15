0eebf9402bc42a30e18a9538b896e179db8ffeb6
Abhinav Shrivastava <shrivastava.abhinav101@gmail.com>
2024-05-15 16:30:56 +0530

    added main.tf file

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-07caf09b362be10b8"
+  instance_type = "t2.micro"
+   tags = {
+    name = "terraform-example1"
+  }
+ 
+}
